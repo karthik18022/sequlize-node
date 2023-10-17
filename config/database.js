e25945c6dd99bca5b8e0bfb1ed89cb113bb5b9af
KarthikRaja <karthikraja.muruganantham@concertidc.com>
2023-10-17 16:44:28 +0530

    Added liquibase

--- config/database.js
+++ config/database.js
@@ -1,4 +1,6 @@
 const Sequlize = require("sequelize");
+const {  Liquibase, POSTGRESQL_DEFAULT_CONFIG } = require('liquibase');
+
 
 const sequelize = new Sequlize(
     'quiz',
@@ -7,7 +9,26 @@ const sequelize = new Sequlize(
        dialect: 'postgres',
        port: process.env.PSQL_PORT,
        host: process.env.PSQL_HOST,  
+
     }
 );
+
+
+const myConfig = {
+	...POSTGRESQL_DEFAULT_CONFIG,
+	url: 'jdbc:postgresql://localhost:5432/quiz',
+	username: 'postgres',
+	password: 'demo',
+    changeLogFile: './changesets/changelog.xml',
+
+}
+const instance = new Liquibase(myConfig);
+
+async function doEet() {
+	await instance.status();
+	await instance.update();
+}
+
+doEet();
 
 module.exports = sequelize; 
